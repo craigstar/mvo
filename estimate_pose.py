bac4bbf68017e46cfa6d6576f36dd8cd70b9b220
Craig <work.craigzhang@gmail.com>
2019-08-31 23:10:54 +0800

    Update camera

--- estimate_pose.py
+++ estimate_pose.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 
-import numpy as np
-import cv2
 import time
+import cv2
+
+from src import Frame, PinholeCamera, Initialization
+
+
 # import matplotlib.pyplot as plt
 # from mpl_toolkits.mplot3d import Axes3D
 
-import src.log
-from src import Frame, PinholeCamera, Initialization
-import src
-
 def getName(k):
     name0 = "%06d" % k
-    name1 = "%06d" % (k+1)
+    name1 = "%06d" % (k + 1)
     sufix = '.png'
     return name0 + sufix, name1 + sufix
+
 
 ####### 1.0 read images #######
 path = '../../dataset/sequences/00/image_0/'
@@ -31,7 +31,7 @@ for i in range(1):
 
     ####### 2.0 create camera, frames #######
     cam = PinholeCamera(width=cols, height=rows,
-                        fx=718.856, fy=718.856, 
+                        fx=718.856, fy=718.856,
                         cx=607.1928, cy=185.2157
                         )
 
@@ -43,7 +43,7 @@ for i in range(1):
     init.add_first_frame(first_frame)
     e = time.time()
     init.add_second_frame(second_frame)
-    print('time elapsed:', e-s)
+    print('time elapsed:', e - s)
 
     # pts = np.array([ft.point.pos for ft in second_frame.features if ft.point is not None])
     # x, y, z = pts[:, 0], pts[:, 1], pts[:, 2]
--- src/camera.py
+++ src/camera.py
@@ -2,76 +2,80 @@ import numpy as np
 
 from .log import LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_CRITICAL
 
-class AbstractCamera(object):
-	"""docstring for AbstractCamera"""
-	def __init__(self, width, height):
-		self.width = width
-		self.height = height
-		LOG_INFO('Camera width:', width, 'height:', height)
 
-	def is_in_frame(self, uv, boundary=0):
-		ll = np.array([boundary, boundary])  							# lower-left
-		ur = np.array([self.width - boundary, self.height - boundary])  # upper-right
-		return np.all(np.logical_and(ll <= uv, uv <= ur))
+class AbstractCamera(object):
+    """docstring for AbstractCamera"""
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        LOG_INFO('Camera width:', width, 'height:', height)
+
+    def is_in_frame(self, uv, boundary=0):
+        ll = np.array([boundary, boundary])  # lower-left
+        ur = np.array([self.width - boundary, self.height - boundary])  # upper-right
+        return np.all(np.logical_and(ll <= uv, uv <= ur))
+
 
 class PinholeCamera(AbstractCamera):
-	"""docstring for PinholeCamera"""
-	def __init__(self, width, height, fx, fy, cx, cy, k1=0.0, k2=0.0, p1=0.0, p2=0.0, k3=0.0):
-		super(PinholeCamera, self).__init__(width, height)
-		self.fx = fx
-		self.fy = fy
-		self.cx = cx
-		self.cy = cy
-		
-		self.is_distorted = abs(k1) > 0.0000001
-		self.cvK = np.array([fx, 0, cx, 0, fy, cy, 0, 0, 1], dtype=float).reshape(3,3)
-		self.cvD = np.array([k1, k2, p1, p2, k3], dtype=float)
-		LOG_INFO('Pinhole Camera fx:', fx, 'fy:', fy, 'cx:', cx, 'cy:', cy)
-		if self.is_distorted:
-			LOG_INFO('Distortion:', self.cvD)
-		else:
-			LOG_INFO('No distortion')
+    """docstring for PinholeCamera"""
 
-	def get_focal_length(self):
-		return abs(self.fx)
+    def __init__(self, width, height, fx, fy, cx, cy, k1=0.0, k2=0.0, p1=0.0, p2=0.0, k3=0.0):
+        super(PinholeCamera, self).__init__(width, height)
+        self.fx = fx
+        self.fy = fy
+        self.cx = cx
+        self.cy = cy
 
-	def K(self):
-		return np.array([
-				[self.fx, 0, self.cx],
-				[0, self.fy, self.cy],
-				[0, 0, 1],
-			])
-		
-	def cam2world(self, uv):
-		if not self.is_distorted:
-			x = (uv[0] - self.cx) / self.fx
-			y = (uv[1] - self.cy) / self.fy
-			z = 1.0
-		else:
-			# TODO: distorted
-			pass
-		v = np.array([x, y, z])
-		return v / np.linalg.norm(v)
+        self.is_distorted = abs(k1) > 0.0000001
+        self.cvK = np.array([fx, 0, cx, 0, fy, cy, 0, 0, 1], dtype=float).reshape(3, 3)
+        self.cvD = np.array([k1, k2, p1, p2, k3], dtype=float)
+        LOG_INFO('Pinhole Camera fx:', fx, 'fy:', fy, 'cx:', cx, 'cy:', cy)
+        if self.is_distorted:
+            LOG_INFO('Distortion:', self.cvD)
+        else:
+            LOG_INFO('No distortion')
 
-	def world2cam(self, pos):
-		pos = np.asarray(pos, dtype=float)
-		
-		if len(pos) == 3:
-			pos = pos[:2] / pos[2]
-		
-		if not self.is_distorted:
-			x = self.fx * pos[0] + self.cx
-			y = self.fy * pos[1] + self.cy
-		else:
-			# TODO: distorted
-			pass
+    def get_focal_length(self):
+        return abs(self.fx)
 
-		return np.array([x, y])
+    def K(self):
+        return np.array([
+            [self.fx, 0, self.cx],
+            [0, self.fy, self.cy],
+            [0, 0, 1],
+        ])
 
-	def compose_P(self, R, t):
-	    RT = np.hstack((R, t))
-	    return np.matmul(self.K(), RT)
+    def cam2world(self, uv):
+        if not self.is_distorted:
+            x = (uv[0] - self.cx) / self.fx
+            y = (uv[1] - self.cy) / self.fy
+            z = 1.0
+        else:
+            # TODO: distorted
+            pass
+        v = np.array([x, y, z])
+        return v / np.linalg.norm(v)
 
-	def undistort_image(self):
-		# TODO
-		pass
+    def world2cam(self, pos):
+        pos = np.asarray(pos, dtype=float)
+
+        if len(pos) == 3:
+            pos = pos[:2] / pos[2]
+
+        if not self.is_distorted:
+            x = self.fx * pos[0] + self.cx
+            y = self.fy * pos[1] + self.cy
+        else:
+            # TODO: distorted
+            pass
+
+        return np.array([x, y])
+
+    def compose_P(self, R, t):
+        RT = np.hstack((R, t))
+        return np.matmul(self.K(), RT)
+
+    def undistort_image(self):
+        # TODO
+        pass
